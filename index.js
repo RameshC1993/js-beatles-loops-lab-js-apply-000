dbcc397a88f38c8b797113c244fbcaf21802c546
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-06-10 01:37:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var arr = [];
+  var str = "";
+  
+  for (let i = 0; i < musicians.length; i++) {
+    str = musicians[i] + " plays " + instruments[i];
+    arr.push(str);
+  }
+  
+  return arr;
+}
+
+
+function johnLennonFacts (facts) {
+    var arr = [];
+    var str;
+
+    let i = 0;
+
+    while(i < facts.length){
+        i++;
+        str = facts[i] + "!!!";
+        arr.push(str)
+
+    }
+    return arr;
+}
+
+
+function iLoveTheBeatles (num) {
+  var myarr = [];
+  var str = "";
+  
+  do{
+    myarr.push("I Love the Beatles!");
+    num++;
+    
+  }while(num < 15);
+    return myarr;
+}
